309998fb4f5bdc4492e990465f077a40c9d0379c
GuilhermeNepomuceno <gngm217@gmail.com>
2021-06-05 18:47:31 -0300

    configuração das dependências

---
+++ animacoes/src/main/java/com/digitalinnovationone/animacoes/AnimacoesApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.animacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimacoesApplication.class, args);
+	}
+
+}
---
+++ animacoes/src/test/java/com/digitalinnovationone/animacoes/AnimacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.animacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
